8eb97f28a6accd0bbc0c3c2efc1d896c57bf8115
thalals <thalsal@naver.com>
2021-08-11 23:52:11 +0900

    Templates DIR 변경 및 url 연결

--- safetymap/main/urls.py
+++ safetymap/main/urls.py
@@ -13,12 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('main.urls')),
-    
+   path('',views.home, name='home'),
 ]
--- safetymap/safetymap/settings.py
+++ safetymap/safetymap/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'safetymap.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")], # 폴더 바깥으로 templates 통합
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
