aa45f2f84f72ea7fb3144df18b787d2f6ceb8791
thalals <thalsal@naver.com>
2021-08-13 22:33:16 +0900

    장소 검색 input, 길찾기 button

--- safetymap/main/views.py
+++ safetymap/main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from folium import plugins
 import folium
 import geocoder     #import geojson
 
@@ -6,6 +7,9 @@ g = geocoder.ip('me')   #현재 내위치
 # Create your views here.
 def home(request) :
     map = folium.Map(location=g.latlng,zoom_start=15, width='100%', height='100%',)
-    maps=map._repr_html_()  #지도를 템플릿에 삽입하기위해 iframe이 있는 문자열로 반환 
+    plugins.LocateControl().add_to(map)
+    # plugins.Geocoder().add_to(map)
+
+    maps=map._repr_html_()  #지도를 템플릿에 삽입하기위해 iframe이 있는 문자열로 반환 (folium)
 
     return render(request,'../templates/home.html',{'map' : maps})
