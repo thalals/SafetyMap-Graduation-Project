0e82c63e617159759d8e2e28c466f7f1241f3f8a
thalals <thalsal@naver.com>
2021-08-17 00:17:23 +0900

    js, css static dir setting

--- safetymap/safetymap/settings.py
+++ safetymap/safetymap/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static') # 개발자 관리 파일들을 static에 저장.
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
---
+++ safetymap/static/js/home.js
@@ -0,0 +1,8 @@
+"use strict"
+var input = document.getElementById("start_input");
+
+input.onclick  = function(){
+    alert('눌렀다ㅇ');
+};
+
+console.log('start');
